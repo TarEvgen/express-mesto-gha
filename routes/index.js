692f86542b40bb8864c50ef8c40fc224207af1d0
Evgenii Tarasov <mailtarasov@yandex.ru>
2023-06-15 19:48:04 +0700

    add err

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
-
+/*
 router.use('*', (req, res) => {
   res.status(404).send({message: 'Страница не найдена'});
-});
+});*/
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
