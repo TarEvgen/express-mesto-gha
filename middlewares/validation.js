8f224f33c984bf3354654e5360de8175bed1401f
Tarasov Evgeniy <mailtarasov@yandex.ru>
2023-07-01 13:54:21 +0700

    правка автотесты валидация аватар

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -4,7 +4,8 @@ const checkBodyLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().required().pattern(/(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/),
+    about: Joi.string().min(2).max(30),   
   }),
 });
 
